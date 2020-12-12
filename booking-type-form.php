13a6195f58afefd7790a23284bb19e1d34efca89
kjvaja <44861319+kjvaja@users.noreply.github.com>
2020-12-12 14:24:40 +0530

    form design complete

--- booking-type-form.php
+++ booking-type-form.php
@@ -1,29 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Booking Type Form</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/boostrap.min.js"></script>
+    <title>Booking Type Form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/time-slot-css.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/boostrap.min.js"></script>
+    
 </head>
 <body>
 
 <div class="container">
-  <h2>Vertical (basic) form</h2>
-  <form action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" name="remember"> Remember me</label>
-    </div>
+    <h2>Booking Type Form </h2>
+    <form>
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="mobileNumber">Mobile Number:</label>
+            <input type="text" class="form-control" id="mobileNumber" placeholder="Enter Mobile Number" name="mobile number" required>
+        </div>
+        <div class="form-group">
+            <label for="date">Pick a Date </label>
+            <input type="date" class="form-control" name="date" id="date" required>
+        </div>
+        <div class="form-group">
+            <label for="time">Select a time:</label>
+            <input type="time" id="time" name="time" required>
+        </div>
+        <div class="form-group">
+            <label for="comments">Wtire Comments</label>
+            <textarea class="form-control" id="comments" name="comments" rows="5" cols="10" required></textarea>
+        </div>
+
+    
     <button type="submit" class="btn btn-default">Submit</button>
   </form>
 </div>
